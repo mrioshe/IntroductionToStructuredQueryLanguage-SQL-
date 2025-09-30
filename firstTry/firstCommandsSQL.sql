c13fa7fa510049239102a23c4d6a8c1c2f6276ff
mrioshe <mrioshe@unal.edu.co>
2025-09-30 09:42:42 -0500

    Update firstCommandsSQL.sql

--- firstTry/firstCommandsSQL.sql
+++ firstTry/firstCommandsSQL.sql
@@ -1,3 +1,12 @@
+INSERT INTO Users (name,email) VALUES ('Ted', 'ted@umich.edu');
+DELETE FROM Users WHERE EMAIL='ted@umich.edu'
 UPDATE users SET name='Charles' WHERE email='csev@umich.edu';
 SELECT * FROM users; 
 SELECT * FROM users WHERE email='csev@umich.edu';
+SELECT * FROM users WHERE name='Alex';
+SELECT * FROM users ORDER BY email;
+SELECT * FROM users where name like '%e%';
+SELECT * FROM users ORDER BY email DESC limit 2;
+SELECT * FROM users ORDER BY email DESC limit 1,2;
+SELECT COUNT(*) FROM  Users;
+SELECT COUNT(*) FROM Users WHERE email='csev@umich.edu'
